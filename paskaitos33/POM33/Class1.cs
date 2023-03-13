a338b3891faa4cc7ad545d7427c58e9ae350704d
Milda <Milda@LAPTOP-3IFJACBT>
2023-03-13 18:57:09 +0200

    Add project files.

---
+++ paskaitos33/POM33/Class1.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using OpenQA.Selenium.DevTools.V108.CSS;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Interactions;
+using System.Data;
+
+namespace paskaitos33.POM33
+{
+    internal class Class1
+    {
+
+        IWebDriver driver;
+        //generalMethods32 genmethods;
+        public Class1(IWebDriver driver)
+        {
+            this.driver = driver;
+           // genmethods = new generalMethods32(driver);
+        }
+    }
+}
---
+++ paskaitos33/paskaita33.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using OpenQA.Selenium.DevTools.V108.CSS;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Interactions;
+using System.Data;
+
+namespace paskaitos33
+{
+    internal class paskaita33
+    {
+        static IWebDriver driver;
+        [SetUp]
+        public static void SETUP()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("--disable-notifications"); // to disable notification
+            driver = new ChromeDriver(options);
+
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://www.1a.lt/";
+            //laukimas kol elementai uzsikraus:
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+            Thread.Sleep(1000);
+            driver.FindElement(By.XPath("//a[@class='c-button']")).Click();
+        }
+        [TearDown]
+        public static void TearDown()
+        {
+            //driver.Quit();
+        }
+    }
+}
